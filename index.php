595d3ce5d1f9eed3787fc7a8d0f7f2b9a5cf5918
MUHAMMAD RANDY KARNA <muhammadrandykarna@gmail.com>
2026-05-09 12:05:07 +0700

    route utama index

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-// Redirect otomatis ke halaman login POS
-header("Location: auth/index.php");
-exit;
+session_start();
+
+// PASTIKAN INI SESUAI DENGAN NAMA FOLDER PROJECT-MU
+$base_url = 'http://localhost/pos-lovecakes/'; 
+
+// Jika sudah ada sesi login aktif, arahkan ke Kasir
+if (isset($_SESSION['pos_user_id'])) {
+    header("Location: " . $base_url . "pos/kasir/");
+    exit;
+} else {
+    // Jika belum login, arahkan ke halaman Login
+    header("Location: " . $base_url . "auth/");
+    exit;
+}
 ?>
